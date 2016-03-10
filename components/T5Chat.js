1b826a6d96a73fc5766caa1abc357f2261b9ef1b
jcc2303 <cabrera.javier@gmail.com>
2016-03-10 20:20:26 +0100

    login google android

--- components/T5Chat.js
+++ components/T5Chat.js
@@ -1,20 +1,24 @@
-import codePush from 'react-native-code-push';
 
 import React, {
     Component,
     StyleSheet,
     Text,
     TextInput,
-    View
+    View,
+    TouchableOpacity,
+    TouchableHighlight
 } from 'react-native';
 import Button from 'react-native-button';
 
 
-import Container from './Container';
-import NewChat from './NewChat';
-
+import codePush from 'react-native-code-push';
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 //import Firebase from 'firebase';
 import Rebase from 're-base';
+
+
+import Container from './Container';
+import NewChat from './NewChat';
 
 
 let base = Rebase.createClass('https://t5-chat.firebaseio.com/');
@@ -24,18 +28,18 @@ export default class T5Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            user: null
+            contacts: [],
             messages: []
         };
-    }
-
-    componentDidMount(params) {
-        console.log('componentDidMount ', params);
-        codePush.sync({
-            updateDialog: true,
-            installMode: codePush.InstallMode.INMEDIATE
-        }, function (status) {
-            console.log('codepush ', status);
-        });
+        
+        GoogleSignin.configure({
+            //scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
+            //iosClientId: <FROM DEVELOPPER CONSOLE>, // only for iOS
+            webClientId: '500862723918-6h0s6ivdnaof6vugvi1c9p8snr6r7e61.apps.googleusercontent.com', //<FROM DEVELOPPER CONSOLE>, // client ID of type WEB for your server (needed to verify user ID and offline access)
+            offlineAccess: true // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        });        
+        
     }
 
     componentWillMount() {
@@ -45,6 +49,13 @@ export default class T5Chat extends React.Component {
          * our local 'messages' state whenever our 'chats'
          * Firebase endpoint changes.
          */
+                
+        base.bindToState('users', {
+            context: this,
+            state: 'contacts',
+            asArray: true
+        });        
+        
         base.bindToState('chats', {
             context: this,
             state: 'messages',
@@ -52,15 +63,69 @@ export default class T5Chat extends React.Component {
         });
 
     }
+    
+    componentDidMount(params) {
+        console.log('componentDidMount ', params);
+        codePush.sync({
+            updateDialog: true,
+            installMode: codePush.InstallMode.INMEDIATE
+        }, function (status) {
+            console.log('codepush ', status);
+        });
+        
+        GoogleSignin.currentUserAsync().then((user) => {
+            console.log('USER', user);
+            this.setState({user: user});
+        }).done();
+    }
 
+    _signIn(){
+        console.log('pressed login');
+        
+        GoogleSignin.signIn()
+        .then((user) => {
+            console.log(user);
+           this.setState({user: user});
+        })
+        .catch((err) => {
+            console.log('WRONG SIGNIN', err);
+        })
+        .done();        
+    }
+    
+    _signOut() {
+        GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+        this.setState({user: null});
+        })
+        .done();
+    }
+  
     render() {
-        return (
-            <View style={ styles.container } >
-                <Text>T5 Chat 2</Text>
-                <NewChat chats={ this.state.messages } />
-                < Container />
-            </View>            
-        )
+        const user = GoogleSignin.currentUser();
+        console.log('user: ',user);
+        console.log('state: ',this.state);
+        
+        if (!this.state.user) {
+            return (
+                <GoogleSigninButton
+                    style={styles.login}
+                    onPress={this._signIn.bind(this)}/>                
+            )
+            
+        } else {            
+            return (
+                <View style={ styles.container } >
+                    <TouchableOpacity onPress={() => {this._signOut(); }} style={styles.logout}>
+                        <Text>Log out</Text>
+                    </TouchableOpacity>
+                          
+                    <Text>T5 Chat 2</Text>
+                    <NewChat chats={ this.state.messages } />
+                    < Container />
+                </View>            
+            )
+        }
+        
     }
 
 }
@@ -71,6 +136,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    login: {
+        backgroundColor: '#F5FCFF',
+        width: 48, 
+        height: 48
+    },
+    logout: {
+        marginTop: 50,
+        width: 48, 
+        height: 48
     },
     welcome: {
         fontSize: 20,
