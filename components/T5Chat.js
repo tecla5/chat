bd9de406a1a3e236c8ed83066aaeff0e6536078b
Kristian Mandrup <kmandrup@gmail.com>
2016-03-13 20:15:25 +0100

    works for iOS

--- components/T5Chat.js
+++ components/T5Chat.js
@@ -28,14 +28,14 @@ export default class T5Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: null
+            user: null,
             contacts: [],
             messages: []
         };
         
         GoogleSignin.configure({
             //scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
-            //iosClientId: <FROM DEVELOPPER CONSOLE>, // only for iOS
+            iosClientId: 'com.tecla5.chat', // only for iOS
             webClientId: '500862723918-6h0s6ivdnaof6vugvi1c9p8snr6r7e61.apps.googleusercontent.com', //<FROM DEVELOPPER CONSOLE>, // client ID of type WEB for your server (needed to verify user ID and offline access)
             offlineAccess: true // if you want to access Google API on behalf of the user FROM YOUR SERVER
         });        
@@ -109,16 +109,20 @@ export default class T5Chat extends React.Component {
             return (
                 <GoogleSigninButton
                     style={styles.login}
+                    size={GoogleSigninButton.Size.Icon}
+                    color={GoogleSigninButton.Color.Dark}
                     onPress={this._signIn.bind(this)}/>                
-            )
+                            )
             
         } else {            
             return (
                 <View style={ styles.container } >
-                    <TouchableOpacity onPress={() => {this._signOut(); }} style={styles.logout}>
+                    <TouchableOpacity onPress={() => {this._signOut(); }}>
+                      <View style={{marginTop: 50}}>
                         <Text>Log out</Text>
-                    </TouchableOpacity>
-                          
+                      </View>
+                    </TouchableOpacity>                          
+
                     <Text>T5 Chat 2</Text>
                     <NewChat chats={ this.state.messages } />
                     < Container />
